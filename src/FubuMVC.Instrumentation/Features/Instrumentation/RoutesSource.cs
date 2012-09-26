6bd717b5118f91cad56b20f94278a88c6662e0e1
Ryan Hauert <ryan.hauert@extendhealth.com>
2012-09-26 10:07:29 -0600

    Urls for route instrumentation details are generated correctly.

--- src/FubuMVC.Instrumentation/Features/Instrumentation/RoutesSource.cs
+++ src/FubuMVC.Instrumentation/Features/Instrumentation/RoutesSource.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using FubuMVC.Core.Urls;
 using FubuMVC.Instrumentation.Features.Instrumentation.Models;
 using FubuMVC.Instrumentation.Runtime;
 using FubuMVC.SlickGrid;
@@ -9,15 +10,19 @@ namespace FubuMVC.Instrumentation.Features.Instrumentation
     public class RoutesSource : IGridDataSource<RouteInstrumentationModel>
     {
         private readonly IInstrumentationReportCache _cache;
+        private readonly IUrlRegistry _urls;
 
-        public RoutesSource(IInstrumentationReportCache cache)
+        public RoutesSource(IInstrumentationReportCache cache, IUrlRegistry urls)
         {
             _cache = cache;
+            _urls = urls;
         }
 
         public IEnumerable<RouteInstrumentationModel> GetData()
         {
-            return _cache.Select(log => new RouteInstrumentationModel(log))
+            return _cache.Select(log => new RouteInstrumentationModel(log){
+                ReportUrl = _urls.UrlFor(new InstrumentationRouteDetailsInputModel { Id = log.Id })
+                })
                 .OrderByDescending(x => x.ExceptionCount)
                 .ThenByDescending(x => x.MaxExecution)
                 .ToList();
