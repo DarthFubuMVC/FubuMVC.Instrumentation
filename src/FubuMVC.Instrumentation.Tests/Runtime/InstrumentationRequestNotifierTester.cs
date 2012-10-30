76f2c817c1502f2bd7505f364642e55702c54832
Andrew Kharlamov <kharlamov@gmail.com>
2012-10-30 14:36:00 -0600

    InstrumentationRequestNotifier tests.

---
+++ src/FubuMVC.Instrumentation.Tests/Runtime/InstrumentationRequestNotifierTester.cs
@@ -0,0 +1,36 @@
+﻿using FubuMVC.Diagnostics.Runtime;
+using FubuMVC.Instrumentation.Runtime;
+using FubuTestingSupport;
+using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace FubuMVC.Instrumentation.Tests.Runtime
+{
+    public class InstrumentationRequestNotifierTester : InteractionContext<InstrumentationRequestNotifier>
+    {
+        private RequestLog _log;
+
+        protected override void beforeEach()
+        {
+            _log = new RequestLog();
+        }
+
+        [Test]
+        public void Should_store_report_on_completed()
+        {
+            ClassUnderTest.Completed(_log);
+
+            MockFor<IInstrumentationReportCache>()
+                .AssertWasCalled(x => x.Store(_log));
+        }
+
+        [Test]
+        public void Should_not_store_report_on_started()
+        {
+            ClassUnderTest.Started(_log);
+
+            MockFor<IInstrumentationReportCache>()
+                .AssertWasNotCalled(x => x.Store(_log));
+        }
+    }
+}
